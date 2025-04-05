29d965a495a3c4cffcaa9c9c84658672f122c819
bmunevar66 <bmunevar@ucundinamarca.edu.co>
2025-04-05 17:18:15 -0500

    Clase Cliente.java
    Se crea la clase cliente donde se definen los atributos, agregando set y get

---
+++ R2-A1-S8 Trivia de Palabras Relacionadas/Cliente.java
@@ -0,0 +1,24 @@
+class Cliente {
+    private String cedula;
+    private String nombre;
+public Clientes(String cedula,String nombre){
+    this.cedula=cedula;
+    this.nombre=nombre;
+}
+public String getCedula(){
+    return cedula;
+}
+public String getNombre(){
+    return nombre;
+}
+public void setCedula(String cedula){
+    this.cedula=cedula;
+}
+public void setNombre(String nombre){
+    this.nombre=nombre;
+}
+@ Override
+public String toString(){
+    return "Cliente{cedula="+cedula+", nombre="+nombre+'}';
+}
+}
